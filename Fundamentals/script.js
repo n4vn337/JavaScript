d7546e2b0a7b8c9eb18d40f8c73db851d15ddafc
Navneet Anand <59524376+n4vn337@users.noreply.github.com>
2023-08-25 01:40:52 +0530

    Taking Decisions

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -1,9 +1,9 @@
 // LECTURE: Values and Variables
-let county = "India";
+let country = "India";
 let continent = "Asia";
 let population = 141
 
-// console.log(county);
+// console.log(country);
 // console.log(continent);
 // console.log(population);
 
@@ -11,8 +11,29 @@ let population = 141
 let isIsland = false;
 let language;
 
-console.log(isIsland);
-console.log(population);
-console.log(county);
-console.log(language);
-console.log(continent);
+// console.log(isIsland);
+// console.log(population);
+// console.log(country);
+// console.log(language);
+// console.log(continent);
+
+// LECTURE: Basic Operators
+// const finlandPopulation = 6;
+
+console.log(population/2);
+console.log(population+1)
+console.log(population>6);
+console.log(33>population);
+// let description = country + ' is in ' + continent + ' , and its ' + population +' million people speak ' + language;
+
+// LECTURE: Strings and Template Literals
+// const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
+// LECTURE: Taking Decisions: if / else Statements
+// population = 130;
+if (population>33) {
+    console.log(`${country}'s population is above average`);
+} else {
+    console.log(`${country}'s population is ${(33-population)/2} million below average`);
+}
+
