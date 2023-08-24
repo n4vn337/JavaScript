16a84f7df29fd7fbbb31b6021dcf9700717b8c90
Navneet Anand <59524376+n4vn337@users.noreply.github.com>
2023-08-24 23:40:55 +0530

    data types

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -1,12 +1,18 @@
 // LECTURE: Values and Variables
-// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
-
 let county = "India";
 let continent = "Asia";
 let population = 141
 
-// 2. Log their values to the console
+// console.log(county);
+// console.log(continent);
+// console.log(population);
 
-console.log(county);
-console.log(continent);
+// LECTURE: Data Types
+let isIsland = false;
+let language;
+
+console.log(isIsland);
 console.log(population);
+console.log(county);
+console.log(language);
+console.log(continent);
