04331212f4ad50971c0050648e8c7a4b194b6a83
Navneet Anand <59524376+n4vn337@users.noreply.github.com>
2023-08-24 23:12:51 +0530

    Add files via upload

---
+++ Fundamentals/script.js
@@ -0,0 +1,12 @@
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+
+let county = "India";
+let continent = "Asia";
+let population = 141
+
+// 2. Log their values to the console
+
+console.log(county);
+console.log(continent);
+console.log(population);
